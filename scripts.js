38427b16fdc0e459d83d14d903489e0f44abcbd5
Jeremy <jeremylin.work@gmail.com>
2025-04-11 04:24:30 -0700

    test

--- scripts.js
+++ scripts.js
@@ -1,88 +1,49 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
- */
-//
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+let currentPageNumber = 0;
+const maxPerPage = 20;
 
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
-
-// This function adds cards the page to display the data in the array
 function showCards() {
   const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML = "";
-  const templateCard = document.querySelector(".card");
+  const defaultCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
-
-    const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
-    cardContainer.appendChild(nextCard); // Add new card to the container
+  for (let i = currentPageNumber; i < currentPageNumber + maxPerPage && i < meals.length; i++) { 
+    const meal = meals[i];
+    const displayCard = defaultCard.cloneNode(true); 
+    createMealCard(displayCard, meal);
+    cardContainer.appendChild(displayCard);
   }
+  currentPageNumber += maxPerPage;
 }
 
-function editCardContent(card, newTitle, newImageURL) {
-  card.style.display = "block";
+function createMealCard(displayCard, meal) {
+  displayCard.style.display = "block";
 
-  const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
+  const cardHeader = displayCard.querySelector("h2");
+  cardHeader.textContent = meal.title;
 
-  const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
+  const cardImage = displayCard.querySelector("img");
+  cardImage.src = meal.image;
+  cardImage.alt = meal.title;
 
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
+  cardImage.addEventListener("click", () => {
+    showIngredientsPanel(meal);
+  });
 }
 
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
+function showIngredientsPanel(meal){
+  const ingredientsPanel = document.getElementById("ingredientsPanel");
+  const title = document.getElementById("mealTitle");
+  const list = document.getElementById("ingredientsList");
+
+  title.textContent = meal.title;
+  list.innerHTML = "";
+  meal.ingredients.forEach(ingredient => {
+    let li = document.createElement("li");
+    li.textContent = ingredient;
+    list.appendChild(li);
+  });
+
+  ingredientsPanel.classList.remove("hidden");
+}
 
 function quoteAlert() {
   console.log("Button Clicked!");
@@ -95,3 +56,18 @@ function removeLastCard() {
   titles.pop(); // Remove last item in titles array
   showCards(); // Call showCards again to refresh
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  showCards();
+  document.getElementById("closeIngredientsPanel").addEventListener("click", () => {
+    document.getElementById("ingredientsPanel").classList.add("hidden");
+  });  
+});
+
+
+window.addEventListener("scroll", () => {
+  const atBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight - 100;
+  if (atBottom) {
+    showCards();
+  }
+});
